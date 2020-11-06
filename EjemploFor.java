d71c36c0d231fb03d1edf9cf579d267e5a416329
raaul23 <73880981+raaul23@users.noreply.github.com>
2020-11-06 10:12:40 +0100

    Ejemplo Java

---
+++ EjemploFor.java
@@ -0,0 +1,13 @@
+package ejemplo.pkgfor;
+
+public class EjemploFor {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 200; i+=2) {
+            System.out.println("Numeros Pares " + i);
+
+        }
+    }
+
+}
